0118b82510b562993f8df489d4eb30f620af2fb0
AleaToir3 <aleatoir3@gmail.com>
2024-10-29 22:21:16 +0000

    add routes on home

--- src/components/category-item/category-Item.components.jsx
+++ src/components/category-item/category-Item.components.jsx
@@ -1,11 +1,12 @@
+import { useNavigate } from 'react-router-dom';
 import './style/category-item.styles.scss';
 
 const CategoryItem = ({category}) => {
-  const {imageUrl,title} = category;
+  const {imageUrl,title,route} = category;
+  const navigate = useNavigate();
+  const navigateHandler = ()=> navigate(route)
   return (
-    <div
-      className="category-container"
-    >
+    <div className="category-container" onClick={navigateHandler}>
       <div
         className="background-image"
         style={{ backgroundImage: `url(${imageUrl})` }}
--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ProductCard from "../product-card/product-card.component";
+import { Link } from "react-router-dom";
 
 const CategoryPreview = ({ title, products })=> {
   return (
     <div className="category-preview-container">
+    <Link to={title}>
        <h2> <span className="title">{title.toUpperCase()}</span> </h2>
+    </Link>
       <div className="products-container">
         {products
           .filter((p, i) => i < 4)
